179ce36acab6042dd03d993d9647cf8d1dd49319
Oluwatoniloba Adekola <atoniloba@gmail.com>
2022-11-02 15:50:40 +0100

    Built the first page that fetches the repos from the github api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Github Repo Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
